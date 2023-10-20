de7c74511205aae9fdcfe22e08784fa5a4dd6e88
Javier Mauricio <javierma36@gmail.com>
2023-10-20 10:48:14 -0500

    Implement closure TicketAPI in main.js file

--- main.js
+++ main.js
@@ -8,17 +8,142 @@
   let total = document.getElementById('total');
   let erase = document.getElementById('borrar');
   let resumen = document.getElementById('resumen');
-  // Object to store information
-  let values = {
-    Nombre: '',
-    Apellido: '',
-    Email: '',
-    Cantidad: 0,
-    Categoria: ''
-  };
-/**
- * [Erase ticket input sale form]
- */
+
+  // Closure implementation
+  // In a real project, this API should go in another file
+  function TicketAPI() {
+    let values = {
+      Nombre: '',
+      Apellido: '',
+      Email: '',
+      Cantidad: 0,
+      Categoria: ''
+    };
+    /**
+     * [Validates if HTMLElement value is empty]
+     * [Scope: Private]
+     * @param {HTMLElement}
+     * @returns {boolean}
+     */
+    function IsEmpty(arg) {
+      return arg.value.length === 0;
+    }
+    /**
+    * [Validate name format using regex and length between 1 and 10 characters]
+    * [Scope: Private]
+    * @param {String}
+    * @returns {boolean}
+    */
+    function IsTextString(arg) {
+      return new RegExp("^([A-Za-z]){1,10}$").test(arg.value);
+    }
+    /**
+     * [Validate if arg HTMLElement.name is in array]
+     * [Scope: Private]
+     * @param {Array} array 
+     * @param {HTMLElement} arg 
+     * @returns {boolean}
+     */
+    function AreFields(array, arg) {
+      return array.filter((x) => { return x == arg.name }).length > 0;
+    }
+    /**
+     * 
+     * @param {*} arg 
+     * @returns {boolean}
+     */
+    function IsPositiveIntegerQuantity(arg) {
+      return (new RegExp(",").test(arg.value)) || (arg.value <= 0) || !(Number.isInteger(Number.parseFloat(arg.value)));
+    }
+    /**
+    * [Message format error in Nombre and Apellido field]
+    * [Scope: Private]
+    * @param {HTMLElement} arg1 [Field to focus on error1]
+    * @param {String} arg2 [HTMLElement string name]
+    */
+    function AlertMsg1(nameField, nameText) {
+      alert(`${nameText} muy extenso (max 10 caracteres) o formato incorrecto`);
+      nameField.focus();
+    }
+    /**
+    * [Message format error if field content is empty]
+    * [Scope: Private]
+    * @param {HTMLElement} arg1 [Field to focus on error1]
+    * @param {String} arg2 [HTMLElement string name]
+    */
+    function AlertMsg2(nameField, nameText) {
+      alert(nameText + " vacio. Digite valor.");
+      nameField.focus();
+    }
+    /**
+    * [Message format error in cantidad field]
+    * [Scope: Private]
+    * @param {HTMLElement} arg1 [Field to focus on error1]
+    * @param {String} arg2 [HTMLElement string name]
+    */
+    function AlertMsg3(nameField, nameText) {
+      alert(`${nameText} debe ser positiva y entera.`);
+      nameField.focus();
+    }
+    /**
+    * [Calculate total ticket cost according to categroy discount]
+    * [Scope: Public]
+    * @param {String} arg1 [Ticket Category: Student, Trainee, Junior] 
+    * @param {Integer} arg2 [Ticket quantity] 
+    * @returns {Integer}
+    */
+    function PrintTotal(category, quantity) {
+      let discount = 0;
+      switch (category) {
+        case "Estudiante":
+          discount = 0.8;
+          break;
+        case "Trainee":
+          discount = 0.5;
+          break;
+        case "Junior":
+          discount = 0.15;
+          break;
+        default:
+          break;
+      }
+      return Math.ceil(200 * quantity * (1 - discount));
+    }
+    /**
+    * Alert user if error found in form or set value in ticket
+    * If return value is > 0, it means there is a validatio error
+    * [Scope: Public]
+    * @param {HTMLElement} arg1 [Field in form] 
+    * @returns {Integer}
+    */
+    function PromtUser(field) {
+      if (IsEmpty(field)) {
+        AlertMsg2(field, field.name);
+        return 1;
+      } else if (AreFields(["Nombre", "Apellido"], field) && !IsTextString(field)) {
+        console.log(IsTextString(field));
+        AlertMsg1(field, field.name);
+        return 1;
+      } else if (AreFields(["Cantidad"], field) && IsPositiveIntegerQuantity(field)) {
+        AlertMsg3(field, field.name);
+        return 1;
+      } else {
+        values[field.name] = field.value;
+        return 0;
+      }
+    }
+
+    let publicAPI = {
+      PromtUser: PromtUser,
+      PrintTotal: PrintTotal
+    };
+    return publicAPI;
+  }
+
+  let ticketForm = TicketAPI();
+  /**
+   * [Erase ticket input sale form]
+   */
   function EraseForm() {
     fname.value = "";
     lname.value = "";
@@ -28,86 +153,17 @@
     total.value = "";
     lname.focus();
   }
-  /**
-   * [Validate name format using regex]
-   * @param {[String]} arg1 [Chars in string between 1 and 10]
-   */
-  function TestString(text) {
-    let regex = new RegExp("^([A-Za-z]){1,10}$");
-    return regex.test(text);
-  }
-  /**
-   * Test format error in Nombre and Apellido field
-   * @param {[HTMLElement]} arg1 [Field to focus on error1] 
-   */
-  function AlertMsg1(nameField) {
-    alert("Nombre muy extenso (max 10 caracteres) o formato incorrecto");
-    nameField.focus();
-  }
-  /**
-   * Test format error in rest of fields
-   * @param {[HTMLElement]} arg1 [Field to focus on errro2]
-   * @param {[String]} arg2 [String to define message in alert]
-   */
-  function AlertMsg2(nameField, nameText) {
-    alert(nameText + " vacio. Digite valor.");
-    nameField.focus();
-  }
-  /**
-   * Alert user if error found in form
-   * @param {[HTMLElement]} arg1 [Field in form] 
-   */
-  function Promtuser(field) {
-    if (field.value === "") {
-      AlertMsg2(field, field.name);
-      return 1;
-    } else if ((["Nombre","Apellido"].filter((x)=>{return x==field.name}).length > 0) && !TestString(field.value)) {
-      AlertMsg1(field);
-      return 1;
-    }else if(field.name == "Cantidad" && ((field.value <= 0) || !Number.isInteger(Number.parseFloat(field.value)))){
-      alert("Cantida debe ser mayro que cero y entera.");
-      total.value = "";
-      return 1;
-    }else{
-      values[field.name] = field.value;
-      return 0;
-    }
-  }
-  /**
-   * Calculate total ticket cost according to categroy discount
-   * @param {[String]} arg1 [Ticket Category: Student, Trainee, Junior] 
-   * @param {[Integer]} arg2 [Ticket quantity] 
-   * @returns 
-   */
-  function PrintTotal(category, quantity){
-    let discount = 0;
-    switch (category) {
-      case "Estudiante":
-        discount = 0.8;
-        break;
-      case "Trainee":
-        discount = 0.5;
-        break;
-      case "Junior":
-        discount = 0.15;
-        break;
-      default:
-        break;
-    }
-    return Math.ceil(200 * quantity * (1-discount));
-  }
-
   // Buttons Event Listeners
   erase.addEventListener('click', EraseForm);
   resumen.addEventListener('click', () => {
     let control = 0;
-    control += Promtuser(category);
-    control += Promtuser(quantity);
-    control += Promtuser(email);
-    control += Promtuser(fname);
-    control += Promtuser(lname);
-    if(control == 0){
-      let totalValue = PrintTotal(category.value, quantity.value)
+    control += ticketForm.PromtUser(category);
+    control += ticketForm.PromtUser(quantity);
+    control += ticketForm.PromtUser(email);
+    control += ticketForm.PromtUser(fname);
+    control += ticketForm.PromtUser(lname);
+    if (control == 0) {
+      let totalValue = ticketForm.PrintTotal(category.value, quantity.value)
       total.value = `Total a pagar: \$ ${totalValue}`;
     }
   });
